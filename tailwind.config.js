d65572c15301bdb99223d181cb126b77a4e6240b
Yozz10 <siestachan1445@gmail.com>
2025-10-28 13:20:37 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        sakura: {
-          50: "#fff0f6",
-          100: "#ffe0ed",
-          200: "#ffc2db",
-          300: "#ff99c4",
-          400: "#ff66a3",
-          500: "#ff3385",
-          600: "#e60073",
-          700: "#b30059",
-          800: "#800040",
-          900: "#4d0026"
-        }
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
